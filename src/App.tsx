68b4bed16e8139d6fb60a735690a5acdc10b976c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-20 22:54:45 +0800

    Import Navbar component and add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-   <div className="bg-gray-100 h-screen">
-      <h1>Hello React TypeScript!</h1>
-   </div>
+    <div className="bg-gray-100">
+      <div className="bg-white w-[90%] h-screen text-gray-700 p-3 m-auto">
+        <Navbar/>
+      </div>   
+    </div>
+   
   )
 }
 
