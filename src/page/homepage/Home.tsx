650301da77e5873004edf491613c4ce309bf9f3a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-21 22:57:48 +0800

    Add flex styling to parent component

--- src/page/homepage/Home.tsx
+++ src/page/homepage/Home.tsx
@@ -1,10 +1,12 @@
+import Projects from "../projects/Projects"
 import Hero from "./Hero"
 
 export default function Home() {
 
   return (
-    <div className="flex-grow py-5 mt-15">
+    <div className="flex-grow py-5 px-35 mt-15 flex flex-col gap-30">
       <Hero />
+      <Projects />
     </div>
   )
 }
