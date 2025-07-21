5b57f75496ac60b5d75742c3dd74f2f153fbcb97
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-21 22:56:06 +0800

    Add position relation to App component parent container. Then edit height to min-h so the navbar will stick even at the bottom of the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Home from "./page/homepage/Home"
 
 function App() {
   return (
-    <div className="bg-gray-100">
-      <div className="bg-white w-[90%] h-screen flex flex-col justify-between text-gray-700 py-3 px-25 m-auto">
+    <div className="bg-gray-100 relative">
+      <div className="bg-white w-[90%] min-h-screen flex flex-col justify-between text-gray-600 py-3 px-25 m-auto">
         <Navbar/>
         <Home />
         <h1>This Footer</h1>
