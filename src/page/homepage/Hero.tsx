c3f0b1b44b9ab80ce81eba77ab67ab87e7824563
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-21 19:08:07 +0800

    Creates Home component where the components in home will be rendered. ALso Hero component

---
+++ src/page/homepage/Hero.tsx
@@ -0,0 +1,55 @@
+
+export default function Hero() {
+
+  return (
+    <div className="flex justify-start items-center gap-10">
+      <div className="h-40 w-40 ml-35">
+        <img 
+          src="/profile.png" 
+          alt="Profile-photo"
+          className="w-full h-full rounded-full"   
+        />
+        </div>
+        <div>
+          <h1 className="font-semibold text-2xl">Bartue.dev | Full Stack Web Developer</h1>
+          <p className="text-base mt-2">I am a full stack developer with expertise <br/> in Typescript, React, Nodejs, Express, PostgreSQL, Prisma</p>
+          
+          <div className="flex items-center gap-3 mt-3">
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg" 
+              className="w-10"
+            />
+
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+              className="w-10"
+            />
+  
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain-wordmark.svg" 
+              className="w-12"
+            />
+          
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" 
+              className="w-10"  
+            />
+            
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" 
+              className="w-10"
+            />
+            
+            <img 
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prisma/prisma-original.svg" 
+              className="w-10"
+            />
+          
+          </div>
+          
+          
+          
+        </div>
+    </div>
+  )
+}
---
+++ src/page/homepage/Home.tsx
@@ -0,0 +1,10 @@
+import Hero from "./Hero"
+
+export default function Home() {
+
+  return (
+    <div className="flex-grow py-5 mt-15">
+      <Hero />
+    </div>
+  )
+}
