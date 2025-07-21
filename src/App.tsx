f0e97aec935990c26d140f95d57cb00e8ab30c70
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-21 19:06:37 +0800

    Add flex flex-col and justify-between so the layout will, Navbar always at top and the footer always at the bottom and the content always in the center using flex grow to its parent container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./common/Navbar"
+import Home from "./page/homepage/Home"
 
 function App() {
   return (
     <div className="bg-gray-100">
-      <div className="bg-white w-[90%] h-screen text-gray-700 p-3 m-auto">
+      <div className="bg-white w-[90%] h-screen flex flex-col justify-between text-gray-700 py-3 px-25 m-auto">
         <Navbar/>
+        <Home />
+        <h1>This Footer</h1>
       </div>   
     </div>
    
