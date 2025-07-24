c59118b8cbb1f86cac00745acdb145cffab5f67a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 21:59:13 +0800

    Added a heading for the Project page

--- src/page/projects/Projects.tsx
+++ src/page/projects/Projects.tsx
@@ -4,6 +4,10 @@ export default function Projects() {
 
   return (
     <div>
+      <div className="mb-4">
+        <h1 className="text-3xl font-bold">Things I’ve Built <span className="italic"> (projects) </span></h1>
+        <p className="mt-2">Through out my learning journey, while having a non-software-developer full time job. I've faced challenges of limited time to focus on developing. But with planning and consistent execution. I've been able to make a progress and bring ideas to life.</p>
+      </div>
       <ProjectCard/>
     </div>
   )
