51733be2e41572b5b6495785960ab392c5d6ce93
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 21:58:24 +0800

    Remove the Project Component and import the ProjectCard instead

--- src/page/homepage/Home.tsx
+++ src/page/homepage/Home.tsx
@@ -1,12 +1,12 @@
-import Projects from "../projects/Projects"
+import ProjectCard from "../../components/projects/ProjectCard"
 import Hero from "./Hero"
 
 export default function Home() {
 
   return (
-    <div className="flex-grow py-5 px-35 mt-15 flex flex-col gap-30">
+    <div className="flex-grow flex flex-col gap-30">
       <Hero />
-      <Projects />
+      <ProjectCard />
     </div>
   )
 }
