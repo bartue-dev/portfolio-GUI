7e75e2cc49dae854d056a08a13ed4c4fc3e6e8a9
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 21:52:26 +0800

    Add the Outlet component from react-router-dom. And import the ScrollToTop component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router"
 import Navbar from "./common/Navbar"
-import Home from "./page/homepage/Home"
+import ScrollToTop from "./common/ScrollToTop"
 
 function App() {
   return (
     <div className="bg-gray-100 relative">
-      <div className="bg-white w-[90%] min-h-screen flex flex-col justify-between text-gray-600 py-3 px-25 m-auto">
+      <ScrollToTop/>
+      <div className="bg-white w-[90%] min-h-screen flex flex-col justify-between text-gray-600 py-3 m-auto">
         <Navbar/>
-        <Home />
+        <div className="px-60 mt-15">
+          <Outlet/>
+        </div>
         <h1>This Footer</h1>
       </div>   
     </div>
