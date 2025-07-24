3c86960bbbb463c02c1f17c48c64b5950082677d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 22:01:05 +0800

    Creates routes file that handles the routing implement of the porfolio

---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import About from "../page/about/About";
+import Contact from "../page/contact/Contact";
+import Home from "../page/homepage/Home";
+import Projects from "../page/projects/Projects";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {index: true, element: <Home/>},
+      {path: "home", element: <Home/>},
+      {path: "projects", element: <Projects/>},
+      {path: "about", element: <About/>},
+      {path: "contact", element: <Contact/>}
+    ]
+  }
+])
