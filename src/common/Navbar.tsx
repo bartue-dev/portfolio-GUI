4bf1d1106aac1f375b245424da0227beae882637
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 21:53:40 +0800

    Remove the links array transfer to data file. Then import the Link component from react-router-dom that allows user to navigate on pages

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -1,26 +1,22 @@
+import { Link } from "react-router"
+import { links } from "../data/data"
 
 export default function Navbar() {
-  
-  const links = [
-    "Home",
-    "Projects",
-    "About",
-    "Contact"
-  ]
-
 
   return (
-    <div className="sticky top-3">
+    <div className="sticky top-3 z-1">
       <nav 
         className="bg-white w-100 m-auto shadow-sm rounded-full py-2 font-semibold text-sm"
       >
         <ul className="flex justify-center items-center gap-10">
           {links.map((link, i) => (
-            <li key={i}
-              className="cursor-pointer hover:text-emerald-600"
+            <Link 
+              to={link.route}
+              key={i}
+              className="hover:text-emerald-600"
             >
-              {link}
-            </li>
+              {link.name}
+            </Link>
           ))}
         </ul>
       </nav>
