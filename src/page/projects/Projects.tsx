d72332b266a1e08cdaa3892032b9287a1e497f57
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-25 22:59:37 +0800

    Added a margin top 15 and edit some words about me

--- src/page/projects/Projects.tsx
+++ src/page/projects/Projects.tsx
@@ -4,9 +4,9 @@ export default function Projects() {
 
   return (
     <div>
-      <div className="mb-4">
+      <div className="mb-4 mt-15">
         <h1 className="text-3xl font-bold">Things I’ve Built <span className="italic"> (projects) </span></h1>
-        <p className="mt-2">Through out my learning journey, while having a non-software-developer full time job. I've faced challenges of limited time to focus on developing. But with planning and consistent execution. I've been able to make a progress and upskill.</p>
+        <p className="mt-2">Through out my learning journey, while having a non-software-developer full time job. I've faced challenges of limited time to focus on developing. But with planning and consistent execution. I've been able to make a quite progress and upskill.</p>
       </div>
       <ProjectCard/>
     </div>
