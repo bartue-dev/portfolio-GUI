b51916915499ba8e4cbb37c01f9d0dc34b8edcab
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-26 20:04:06 +0800

    Added more styling

--- src/page/projects/Projects.tsx
+++ src/page/projects/Projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
   return (
     <div>
       <div className="mb-4 mt-15">
-        <h1 className="text-3xl font-bold">Things I’ve Built <span className="italic"> (projects) </span></h1>
+        <h1 className="text-4xl font-bold text-gray-700">Things I’ve Built <span className="italic"> (projects) </span></h1>
         <p className="mt-2">Through out my learning journey, while having a non-software-developer full time job. I've faced challenges of limited time to focus on developing. But with planning and consistent execution. I've been able to make a quite progress and upskill.</p>
       </div>
       <ProjectCard/>
