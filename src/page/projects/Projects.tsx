fb899c1deaeb20d4abb283d63027be21020fb19d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-27 11:02:21 +0800

    Set up px-20 as default then lg:px-10 to the text container

--- src/page/projects/Projects.tsx
+++ src/page/projects/Projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
 
   return (
     <div>
-      <div className="mb-4 mt-15">
+      <div className="mb-4 mt-15 px-20 lg:px-10">
         <h1 className="text-4xl font-bold text-gray-700">Things I’ve Built <span className="italic"> (projects) </span></h1>
         <p className="mt-2">Through out my learning journey, while having a non-software-developer full time job. I've faced challenges of limited time to focus on developing. But with planning and consistent execution. I've been able to make a quite progress and upskill.</p>
       </div>
