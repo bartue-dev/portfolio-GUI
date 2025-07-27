bae08927399257c2c783a1896466dd55c62d5dfd
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-27 11:40:41 +0800

    Set up px-8 as default then sm:px-20

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <ScrollToTop/>
       <div className="bg-white w-[90%] min-h-screen flex flex-col justify-between text-gray-600 py-3 m-auto">
         <Navbar/>
-        <div className="justify-items-center px-20 lg:px-40">
+        <div className="justify-items-center px-8 sm:px-20 lg:px-40">
           <Outlet/>
         </div>
         <Footer/>
