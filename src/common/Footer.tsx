42935d9618d4a60f28a3755de6ad90ac62d94091
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-27 11:41:37 +0800

    Remove the px-60 from the parent container. Set up text-xs as default then sm:text-sm for the links

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -4,15 +4,15 @@ import { links } from "../data/data"
 export default function Footer() {
 
   return (
-    <div className="border-t-2 border-gray-300 py-3 mt-15 flex items-center sm:px-20 px-60 justify-center lg:justify-between">
+    <div className="border-t-2 border-gray-300 py-3 mt-15 flex items-center sm:px-20 justify-center lg:justify-between">
       <div className="flex justify-center items-center gap-8">
         {links.map((link, i) => (
-          <Link to={link.route} key={i} className="font-semibold text-sm">
+          <Link to={link.route} key={i} className="font-semibold text-xs sm:text-sm">
             {link.name}
           </Link>
         ))}
       </div>
-      <h1 className="text-sm hidden lg:block"> ©{new Date().getFullYear()} bartue.dev All Rights Reserved </h1>
+      <h1 className="sm:text-sm hidden lg:block"> ©{new Date().getFullYear()} bartue.dev All Rights Reserved </h1>
     </div>
   )
 }
