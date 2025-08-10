3c17a6fb7a3b8f2ee34a31e43e7fd1778a9c2d3a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-08-10 09:43:58 +0800

    Import the activity component

--- src/page/homepage/Home.tsx
+++ src/page/homepage/Home.tsx
@@ -1,12 +1,14 @@
+import Activity from "../../components/activities/activity"
 import ProjectCard from "../../components/projects/ProjectCard"
 import Hero from "./Hero"
 
 export default function Home() {
 
   return (
-    <div className="flex-grow flex flex-col gap-30">
+    <div className="flex-grow flex flex-col gap-10">
       <Hero />
       <ProjectCard />
+      <Activity/>
     </div>
   )
 }
