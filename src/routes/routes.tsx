248c83796ac9a337a282f00ec8ad2ffba896700a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-01 14:54:42 +0800

    Import ErrorPage component. As errorElement for the page. 404 error specifically

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,11 +5,13 @@ import Contact from "../page/contact/Contact";
 import Home from "../page/homepage/Home";
 import Projects from "../page/projects/Projects";
 import ProjectDetails from "../components/projects/ProjectDetails";
+import ErrorPage from "../common/Error404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <ErrorPage/>,
     children: [
       {index: true, element: <Home/>},
       {path: "home", element: <Home/>},
