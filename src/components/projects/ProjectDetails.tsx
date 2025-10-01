e12d4a30c39fb272c69bd9b3129c807cfd31578c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-01 21:14:57 +0800

    Added a image fullscreen feature. Escape key can be use to close the fullscreen image. Note: fullscreen image is styled in lightbox modal type

--- src/components/projects/ProjectDetails.tsx
+++ src/components/projects/ProjectDetails.tsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router"
 import { projects } from "../../data/data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight, ImageOff  } from 'lucide-react';
 
 
 export default function ProjectDetails() {
   const { projectId } = useParams();
   const [slideIndex, setSlideIndex] = useState(0);
+  const [isOpenImage, setIsOpenImage] = useState(false)
 
   const nextBtn = (images: string[]) => { 
     setSlideIndex((prev:number) => {
@@ -25,6 +26,36 @@ export default function ProjectDetails() {
       return prev -1
     })
   }
+
+  //open image to fullscreen
+  const openImage = () => {
+    setIsOpenImage(true)
+  }
+
+  //close image from fullscreen
+  const closeImage = () => {
+    setIsOpenImage(false)
+  }
+
+  //use escape key to close the image from fullscreen
+  useEffect(() => {
+    //esc callback function
+    const escBtn = (e : KeyboardEvent) => {
+      if (e.key === "Escape") {
+        closeImage()
+        console.log("Esc btn is press")
+      }
+    }
+
+    //only esc if img is open
+    if (isOpenImage === true) {
+      window.addEventListener("keydown", escBtn)
+    }
+
+    return () => {
+      window.removeEventListener("keydown", escBtn)
+    }
+  },[isOpenImage])
 
   return (
     <div className="mt-15 w-full h-screen">
@@ -60,7 +91,7 @@ export default function ProjectDetails() {
                 {project?.demo && <a href={project?.demo?.link} target="_blank" className="link w-fit"> demo link</a>}
             </div>
 
-            {(project?.images ?? []).length > 0
+            {project?.images && project?.images?.length > 0
               ? <div className="relative w-full h-90 shadow-md border border-gray-200 rounded-md mt-5">
                     <button
                       className="absolute top-1/2 -left-10 cursor-pointer w-8 h-8 border rounded-full flex justify-center items-center hover:border-2"
@@ -71,7 +102,8 @@ export default function ProjectDetails() {
                     <img 
                       src={`/${(project?.images ?? [])[slideIndex]}`} 
                       alt="project-image"
-                      className="w-full h-full object-cover rounded-md"
+                      className="w-full h-full object-cover rounded-md cursor-pointer"
+                      onClick={openImage}
                     />
                     <button
                       className="absolute top-1/2 -right-10 cursor-pointer w-8 h-8 border rounded-full flex justify-center items-center hover:border-2"
@@ -79,6 +111,19 @@ export default function ProjectDetails() {
                     >
                       <ChevronRight/>
                     </button>
+
+                     {isOpenImage && (
+                        <div
+                          className="fixed inset-0 bg-black/50 bg-opacity-80 flex justify-center items-center z-50"
+                          onClick={closeImage}
+                        >
+                          <img
+                            src={`/${(project?.images ?? [])[slideIndex]}`} 
+                            alt="full"
+                            className="max-h-[90%] max-w-[90%] rounded shadow-lg"
+                          />
+                        </div>
+                      )}
                   </div>
                 : <div 
                     className=" w-full h-90 shadow-md border border-gray-200 rounded-md mt-5"
